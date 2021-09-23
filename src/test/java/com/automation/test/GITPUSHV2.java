b23410a32215f3b98181873d8f1b74f0517fa966
KarthikKrishnachar <Karthik.vidic@gmail.com>
2021-09-23 08:25:51 -0400

    Pushing from ES laptop
    Pushing from ES laptop

--- src/test/java/com/automation/test/GITPUSHV2.java
+++ src/test/java/com/automation/test/GITPUSHV2.java
@@ -1,26 +1,13 @@
 package com.automation.test;
 
-
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import com.automation.pages.HomePage;
 import com.automation.utils.BaseTest;
 
-
 public class GITPUSHV2 extends BaseTest {
-	
-	protected HomePage hm = null;
-	
+
 	@Test
 	public void clickonTestAutomation() {
-		helper.startTestCase("clickonTestAutomation");
-		
-		Assert.assertEquals(helper.getPageTitle(driver), asserts[1][1]);
-		hm = new HomePage(driver);
-		hm.clickTestAutomation();
-		helper.info("Navigated to Automation page");
-		helper.endTestCase("clickonTestAutomation");
-		
+
 	}
 }
