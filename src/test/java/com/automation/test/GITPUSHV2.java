7ce24b6ae24707a300992fdd025b0a464f57d6dd
Drugwired <karthik.bluestake1@gmail.com>
2021-09-23 18:08:21 +0530

    commit from desktop adding v3 class file

---
+++ src/test/java/com/automation/test/GITPUSHV2.java
@@ -0,0 +1,13 @@
+package com.automation.test;
+
+import org.testng.annotations.Test;
+
+import com.automation.utils.BaseTest;
+
+public class GITPUSHV2 extends BaseTest {
+
+	@Test
+	public void clickonTestAutomation() {
+
+	}
+}
---
+++ src/test/java/com/automation/test/GITPUSHV3.java
@@ -0,0 +1,13 @@
+package com.automation.test;
+
+import org.testng.annotations.Test;
+
+import com.automation.utils.BaseTest;
+
+public class GITPUSHV3 extends BaseTest {
+
+	@Test
+	public void clickonTestAutomation() {
+
+	}
+}
